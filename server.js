71a595e6e857e03b02749ad345ac5c61fe1e156d
pablolorenzo <pablolorenzopena@gmail.com>
2022-03-27 04:32:38 +0200

    pos sip

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const basicAuth = require('express-basic-auth')
+
+var user = process.env.USER;
+var pass = process.env.PASS;
+
 
 const app = express()
 
 //here we are configuring dist to serve app files
+/*
+app.use(basicAuth({
+    users: { user: pass }
+}))*/
+
+/*app.use(basicAuth({
+    users: { 'admin': 'supersecret' }
+}))*/
+
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
